615619216b3f7bb22d7ccc943cd1550f7853db04
Ivan Dackiewicz <ivandack@gmail.com>
2019-10-05 22:18:52 -0300

    Fixed Roads and Libraries test

--- GraphTheory/RoadsAndLibraries/test.js
+++ GraphTheory/RoadsAndLibraries/test.js
@@ -2,40 +2,43 @@ const path = require('path');
 const fs = require('fs');
 const expect = require('chai').expect;
 const child = require('child_process');
-const exec = 'node ./index.js';
 
-// TODO: Rewrite tests
+const OUTPUT_FILE = path.join(__dirname, './test_output');
+const INPUT_REGEXP = /input(:?[0-9]+).txt/;
+
 describe('Roads and Libraries', () => {
 
-  it.only('should pass test1', function(done) {
-    const proc = child.spawn(exec, {
-      stdio: [fs.openSync('./testCases/test1', 'r'), 'pipe', 'pipe']
+  const inputFiles = fs.readdirSync('./input');
+  const cases = inputFiles.map(f => f.match(INPUT_REGEXP)[1]);
+
+  beforeEach('Set output file', function () {
+    fs.openSync(OUTPUT_FILE, 'w');
+  });
+
+  afterEach('Clean output file', function () {
+    if (fs.existsSync(OUTPUT_FILE)) {
+      fs.unlinkSync(OUTPUT_FILE);
+    }
+  });
+
+  for (let test of cases) {
+
+    const input = fs.readFileSync(`./input/input${test}.txt`, 'utf8');
+    const expectedOutput = fs.readFileSync(`./output/output${test}.txt`, 'utf8');
+    it('should pass test from input' + test, function (done) {
+      const proc = child.spawn('node', ['./index.js'], {
+        env: Object.assign({ OUTPUT_PATH: OUTPUT_FILE }, process.env),
+        stdio: 'pipe',
+      });
+
+      proc.stdout.on('close', function () {
+        const output = fs.readFileSync(OUTPUT_FILE, 'utf8');
+        expect(output.trim()).to.eq(expectedOutput);
+        done();
+      });
+      proc.stdin.write(input);
+      proc.stdin.end();
     });
 
-    console.log(proc);
-    proc.stdout.once('data', function(output) {
-      expect(output.toString('utf-8')).to.eq('4\n12');
-      done();
-    });
-  });
-  it('should pass test2', function(done) {
-    const proc = child.spawn(exec, {
-      stdio: [fs.openSync('./testCases/test2'), 1, 2]
-    });
-
-    proc.stdout.once('data', function(output) {
-      expect(output.toString('utf-8')).to.eq('12');
-      done();
-    });
-  });
-  it('should pass test3', function(done) {
-    const proc = child.spawn(exec, {
-      stdio: [fs.openSync('./testCases/test3'), 1, 2]
-    });
-
-    proc.stdout.once('data', function(output) {
-      expect(output.toString('utf-8')).to.eq('15');
-      done();
-    });
-  });
+  }
 });
